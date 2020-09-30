e1696b5f3e9d267c8173051c220b9307dc6fbbee
demonsome <horseradish1208@gmail.com>
2020-09-30 19:01:14 +0800

    Add files via upload
    Write a C program and try to translate it into RISC-V assembly code. Then, using Ripes simulator to execute the RISC-V assembly code.

---
+++ convolution_sum.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main()
+{
+    int A[5] = { 10,12,31,17,25 };
+    int B[5] = { 3,8,78,91,71 };
+    int N = 4;
+    int C[5];
+
+    for (int i = 0; i < N + 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++) {
+            if (!j) {
+                C[i] = A[j] * B[i - j];
+            }
+            else {
+                C[i] = C[i] + (A[j] * B[i - j]);
+            }
+        }
+        printf("C[%d] = %d\n", i, C[i]);
+    }
+    return 0;
+
+}
